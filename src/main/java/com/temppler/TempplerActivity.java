968b1be9c844db6ab403d2a5756b723967051768
DerGenaue <dergenaue@online.de>
2015-05-28 23:41:02 +0200

    Audio out testing

--- src/main/java/com/temppler/TempplerActivity.java
+++ src/main/java/com/temppler/TempplerActivity.java
@@ -1,17 +1,39 @@
 package com.temppler;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class TempplerActivity extends ActionBarActivity {
+
+
+    private SeekBar valueIn;
+    private TextView valueOut;
+    private boolean done = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+
+        valueIn = (SeekBar) this.findViewById(R.id.valueIn);
+        valueOut = (TextView) this.findViewById(R.id.valueOut);
+
+        valueIn.setOnSeekBarChangeListener(OSBCL);
+        valueIn.setProgress(400);
+        valueOut.setText("0");
+
+        Thread t = new Thread(new OutDACrunnable());
+        t.setPriority(Thread.MAX_PRIORITY);
+        t.start();
     }
 
 
@@ -35,5 +57,79 @@ public class TempplerActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    protected void onStop(){
+        done = true;
+        System.exit(0);
+    }
+
+
+
+    private SeekBar.OnSeekBarChangeListener OSBCL = new SeekBar.OnSeekBarChangeListener(){
+        public void onProgressChanged(SeekBar sb, int progress, boolean u){
+            valueOut.setText(progress+" Hz");
+        }
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
+
+
+
+
+    class OutDACrunnable implements Runnable {
+
+        private short[] generatedSnd;
+        private int sampleR = 44100;
+        private double ph = 0;
+        private int amp = 10000;
+
+
+        public void run() {
+            generatedSnd = new short[AudioTrack.getMinBufferSize(sampleR, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)];
+            System.out.println("Buffer-length: "+ generatedSnd.length);
+            for(int i = 0; i < generatedSnd.length; i++){
+                generatedSnd[i] = 0;
+            }
+
+            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleR,
+                    AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                    generatedSnd.length, AudioTrack.MODE_STREAM);
+
+            audioTrack.play();
+
+            while (! done) {
+                genTone(valueIn.getProgress()); //;valueIn.getProgress());
+
+                //System.out.println(val);
+
+
+                audioTrack.write(generatedSnd, 0, generatedSnd.length);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            audioTrack.release();
+        }
+
+
+        private void genTone(float freq) {
+            for (int i = 0; i < generatedSnd.length; i++) {
+                generatedSnd[i] = (short) (Math.sin(ph) * (double)amp);
+
+
+                ph += 2*Math.PI*freq/sampleR;
+                //ph %= 2*Math.PI;
+
+            }
+            // Log.d(MyTag, "genTone: done");
+        }
     }
 }
